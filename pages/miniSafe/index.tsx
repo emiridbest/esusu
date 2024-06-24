7bb456baffb843ece7a7963a5ac994c9fa536e30
emiridbest <udogwuemiri@gmail.com>
2024-06-24 08:10:50 +0100

    amen again

--- pages/miniSafe/index.tsx
+++ pages/miniSafe/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useCallback, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { contractAddress, abi } from '../../utils/abi';
 import { BrowserProvider, Contract, parseEther, formatUnits } from "ethers";
 import { CurrencyDollarIcon, CurrencyEuroIcon, CurrencyPoundIcon } from "@heroicons/react/24/outline";
@@ -48,6 +50,7 @@ export default function Home() {
         }
       } catch (error) {
         console.error("Error fetching balance:", error);
+        toast.error("Error fetching balance");
       }
     }
   }, []);
@@ -71,6 +74,7 @@ export default function Home() {
         }
       } catch (error) {
         console.error("Error fetching token balance:", error);
+        toast.error("Error fetching token balance");
       }
     }
   }, []);
@@ -79,9 +83,8 @@ export default function Home() {
     setSelectedToken(event.target.value);
   };
 
-  const approveSpend = async (amount: string) => {
+  const approveSpend = async (event: React.FormEvent) => {
     setIsApproving(true);
-    setButtonText('Approving');
     if (window.ethereum) {
       let accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
@@ -91,7 +94,7 @@ export default function Home() {
       const provider = new BrowserProvider(window.ethereum);
       const signer = await provider.getSigner(userAddress);
       const contract = new Contract(contractAddress, abi, signer);
-      const depositValue = parseEther(amount);
+      const depositValue = parseEther(depositAmount.toString());
       const gasLimit = parseInt("600000");
 
       try {
@@ -100,9 +103,11 @@ export default function Home() {
         await tx.wait();
         setIsApproved(true);
         setButtonText('Deposit');
+        toast.success('Approval successful!');
       } catch (error) {
         console.error("Error approving spend:", error);
         setIsApproved(false);
+        toast.error('Approval failed!'); 
       }
     }
     setIsApproving(false);
@@ -115,7 +120,7 @@ export default function Home() {
     setIsWaitingTx(true);
 
     if (selectedToken === 'cUSD' && !isApproved) {
-      await approveSpend(depositAmount.toString());
+      await approveSpend(event);
       if (!isApproved) {
         console.error('Token approval failed');
         setIsWaitingTx(false);
@@ -148,8 +153,16 @@ export default function Home() {
         getTokenBalance();
         setDepositAmount(0);
         setIsApproved(false);
+        setButtonText('Approve');
+        toast.success('Deposit successful!');
       } catch (error) {
         console.error("Error making deposit:", error);
+        toast.error('Deposit failed!');
+        setButtonText('Approve');
+        getBalance();
+        getTokenBalance();
+        setDepositAmount(0);
+        setIsApproved(false);
       }
     }
 
@@ -172,16 +185,22 @@ export default function Home() {
       const withdrawValue = parseEther(withdrawAmount.toString());
       const gasLimit = parseInt("600000");
 
-      if (selectedToken === 'CELO') {
-        let tx = await contract.withdraw(celoAddress, withdrawValue, { gasLimit });
-        await tx.wait();
-      } else if (selectedToken === 'cUSD') {
-        let tx = await contract.withdraw(cUsdTokenAddress, withdrawValue, { gasLimit });
-        await tx.wait();
+      try {
+        if (selectedToken === 'CELO') {
+          let tx = await contract.withdraw(celoAddress, withdrawValue, { gasLimit });
+          await tx.wait();
+        } else if (selectedToken === 'cUSD') {
+          let tx = await contract.withdraw(cUsdTokenAddress, withdrawValue, { gasLimit });
+          await tx.wait();
+        }
+        getBalance();
+        getTokenBalance();
+        setWithdrawAmount(0);
+        toast.success('Withdrawal successful!');
+      } catch (error) {
+        console.error("Error making withdrawal:", error);
+        toast.error('Withdrawal failed!');
       }
-      getBalance();
-      getTokenBalance();
-      setWithdrawAmount(0);
     }
   };
 
@@ -197,15 +216,21 @@ export default function Home() {
       const contract = new Contract(contractAddress, abi, signer);
       const gasLimit = parseInt("600000");
 
-      if (selectedToken === 'CELO') {
-        let tx = await contract.breakTimelock(celoAddress, { gasLimit });
-        await tx.wait();
-      } else if (selectedToken === 'cUSD') {
-        let tx = await contract.breakTimelock(cUsdTokenAddress, { gasLimit });
-        await tx.wait();
+      try {
+        if (selectedToken === 'CELO') {
+          let tx = await contract.breakTimelock(celoAddress, { gasLimit });
+          await tx.wait();
+        } else if (selectedToken === 'cUSD') {
+          let tx = await contract.breakTimelock(cUsdTokenAddress, { gasLimit });
+          await tx.wait();
+        }
+        getBalance();
+        getTokenBalance();
+        toast.success('Timelock broken successfully!');
+      } catch (error) {
+        console.error("Error breaking timelock:", error);
+        toast.error('Error breaking timelock');
       }
-      getBalance();
-      getTokenBalance();
     }
   };
 
@@ -216,6 +241,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4 lg:p-0">
+            <ToastContainer />
       <div className="flex flex-col lg:flex-row text-sm ">
         <aside className="w-full lg:w-1/3 p-4">
           <div className="bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
@@ -241,7 +267,7 @@ export default function Home() {
                   id="token-select"
                   value={selectedToken}
                   onChange={handleTokenChange}
-                  className="w-full border border-gray-300 rounded-md p-2 bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-green-500"
+                  className="w-full rounded-md p-2 shadow bg-prosperity text-black hover:bg-black hover:text-gray-700"
                 >
                   <option value="cUSD"><CurrencyDollarIcon className="mr-2" />cUSD</option>
                   <option value="CELO"><CurrencyDollarIcon className="mr-2" />CELO</option>
@@ -253,61 +279,61 @@ export default function Home() {
         <main className="w-full lg:w-2/3 p-4">
           <div className="bg-gypsum p-6 rounded-lg shadow-md mb-4 bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
             <div className="bg-gypsum p-6 rounded-lg shadow-md mb-4 bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
-            <h3 className="text-sm font-semibold text-black mb-2">Deposit</h3>
-            <form onSubmit={handleDeposit}>
-              <div className="mb-4">
-                <label className="text-sm font-light text-gray-500 mb-2" htmlFor="deposit-amount">Amount</label>
-                <label className="flex text-xs font-light text-gray-500 mb-2" htmlFor="deposit-amount">Approve amount before depositing...</label>
-                <input
-                  id="deposit-amount"
-                  value={depositAmount}
-                  onChange={(e) => setDepositAmount(Number(e.target.value))}
-                  className="w-full border border-gray-300 p-2 rounded-md"
-                  type="text"
-                  placeholder="Amount to deposit"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full bg-black text-white py-2 rounded-md hover:text-gypsum transition"
-              >
-                {isWaitingTx ? <Loader alt /> : buttonText}
-              </button>
-            </form>
-          </div>
-          <div className="bg-gypsum p-6 rounded-lg shadow-md mb-4 bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
-            <h3 className="text-sm font-semibold text-black mb-2">Withdraw</h3>
-            <form onSubmit={handleWithdraw}>
-              <div className="mb-4">
-                <label className="text-sm font-light text-gray-500 mb-2" htmlFor="withdraw-amount">Amount</label>
-                <input
-                  id="withdraw-amount"
-                  value={withdrawAmount}
-                  onChange={(e) => setWithdrawAmount(Number(e.target.value))}
-                  className="w-full border border-gray-300 p-2 rounded-md"
-                  type="text"
-                  placeholder="Amount to withdraw"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full bg-black text-white py-2 rounded-md hover:text-gypsum transition"
-              >
-                Withdraw
-              </button>
-            </form>
-          </div>
-          <div className="bg-gypsum p-6 rounded-lg shadow-md mb-4 bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
-            <h3 className="text-sm font-semibold text-black mb-2">Break Timelock</h3>
-            <h3 className="text-sm font-light text-gray-500 mb-2">Ensure you have enough EST tokens</h3>
+              <h3 className="text-sm font-semibold text-black mb-2">Deposit</h3>
+              <form onSubmit={handleDeposit}>
+                <div className="mb-4">
+                  <label className="text-sm font-light text-gray-500 mb-2" htmlFor="deposit-amount">Amount</label>
+                  <label className="flex text-xs font-light text-gray-500 mb-2" htmlFor="deposit-amount">Approve amount before depositing...</label>
+                  <input
+                    id="deposit-amount"
+                    value={depositAmount}
+                    onChange={(e) => setDepositAmount(Number(e.target.value))}
+                    className="w-full border border-gray-300 p-2 rounded-md"
+                    type="text"
+                    placeholder="Amount to deposit"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-prosperity shadow text-black py-2 rounded-md hover:bg-black hover:text-white transition"
+                >
+                  {isWaitingTx ? <Loader alt /> : buttonText}
+                </button>
+              </form>
+            </div>
+            <div className="bg-gypsum p-6 rounded-lg shadow-md mb-4 bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
+              <h3 className="text-sm font-semibold text-black mb-2">Withdraw</h3>
+              <form onSubmit={handleWithdraw}>
+                <div className="mb-4">
+                  <label className="text-sm font-light text-gray-500 mb-2" htmlFor="withdraw-amount">Amount</label>
+                  <input
+                    id="withdraw-amount"
+                    value={withdrawAmount}
+                    onChange={(e) => setWithdrawAmount(Number(e.target.value))}
+                    className="w-full border border-gray-300 p-2 rounded-md"
+                    type="text"
+                    placeholder="Amount to withdraw"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-prosperity shadow text-black py-2 rounded-md hover:bg-black hover:text-white transition"
+                >
+                  Withdraw
+                </button>
+              </form>
+            </div>
+            <div className="bg-gypsum p-6 rounded-lg shadow-md mb-4 bg-gradient-to-br from-gypsum to-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-lg border border-gray-300 rounded-lg shadow-lg">
+              <h3 className="text-sm font-semibold text-black mb-2">Break Timelock</h3>
+              <h3 className="text-sm font-light text-gray-500 mb-2">Ensure you have enough EST tokens</h3>
 
-            <button
-              onClick={handleBreakLock}
-              className="w-full bg-black text-white py-2 rounded-md  hover:text-gypsum transition"
-            >
-Breaklock            </button>
+              <button
+                onClick={handleBreakLock}
+                className="w-full bg-prosperity shadow text-black py-2 rounded-md hover:bg-black hover:text-white transition"
+              >
+                Breaklock            </button>
             </div>
-            </div>
+          </div>
         </main>
       </div>
     </div>
