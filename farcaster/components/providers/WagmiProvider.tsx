b1c58dc32601a24b8a2bd0a8d30a431be39306e1
emiridbest <udogwuemiri@gmail.com>
2025-05-24 15:08:43 +0100

    fix:call farcaster connector to sign transaction, display connected address

--- farcaster/components/providers/WagmiProvider.tsx
+++ farcaster/components/providers/WagmiProvider.tsx
@@ -1,14 +1,15 @@
 import { createConfig, http, injected, WagmiProvider } from "wagmi";
-import { celo} from "wagmi/chains";
+import { celo } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { farcasterFrame as miniAppConnector } from '@farcaster/frame-wagmi-connector'
 
+// Create a custom config with error handling
 export const config = createConfig({
   chains: [celo],
   transports: {
     [celo.id]: http(),
   },
-  connectors: [farcasterFrame(), injected()],
+  connectors: [ miniAppConnector(), injected() ],
 });
 
 const queryClient = new QueryClient();
