59b2ac1bb12a773c8b0f9896ef91228e788a2f0a
emiridbest <udogwuemiri@gmail.com>
2025-05-24 12:10:39 +0100

    fix: remove redundant nav items

--- farcaster/components/Footer.tsx
+++ farcaster/components/Footer.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { 
   HomeIcon, 
-  ArchiveBoxArrowDownIcon, 
-  UserGroupIcon, 
-  UserIcon 
+ // ArchiveBoxArrowDownIcon, 
+ // UserGroupIcon, 
+ // UserIcon 
 } from "@heroicons/react/24/outline";
 import { cn } from "../lib/utils";
 import { useRouter, usePathname } from "next/navigation";
@@ -16,7 +16,7 @@ export default function Footer() {
   
   const navItems = [
     { name: "Home", icon: HomeIcon, path: "/", ariaLabel: "Navigate to home" },
-   { name: "Utility Bills", icon: ArchiveBoxArrowDownIcon, path: "/", ariaLabel: "Navigate to utility bills" },
+   //{ name: "Utility Bills", icon: ArchiveBoxArrowDownIcon, path: "/", ariaLabel: "Navigate to utility bills" },
    // { name: "Save", icon: ArchiveBoxArrowDownIcon, path: "/miniSafe", ariaLabel: "Navigate to savings" },
    // { name: "Thrift", icon: UserGroupIcon, path: "/thrift", ariaLabel: "Navigate to thrift" },
    // { name: "Profile", icon: UserIcon, path: "/profile", ariaLabel: "Navigate to profile" }
@@ -24,7 +24,7 @@ export default function Footer() {
 
   return (
     <footer className="sm:hidden fixed bottom-0 w-full backdrop-blur-md bg-white/70 dark:bg-black/70 border-t border-gray-200 dark:border-gray-800 shadow-lg z-40">
-      <div className="flex justify-around py-3">
+      <div className="flex justify-around py-3 text">
         {navItems.map((item) => {
           const Icon = item.icon;
           const active = isActive(item.path);
@@ -37,7 +37,7 @@ export default function Footer() {
                 "flex flex-col items-center space-y-1 transition-all duration-200 hover:scale-110",
                 "text-xs focus:outline-none",
                 active 
-                  ? "text-primary" 
+                  ? "text-black dark:text-primary font-semibold" 
                   : "text-gray-600 dark:text-gray-400 hover:text-primary dark:hover:text-primary"
               )}
               aria-label={item.ariaLabel}
