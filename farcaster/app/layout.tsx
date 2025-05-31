9b8fd1dbdb56c5b3683014545f49ba893459bbaf
emiridbest <udogwuemiri@gmail.com>
2025-05-31 17:08:21 +0100

    feat: wrap children with claim provider context via layout

--- farcaster/app/layout.tsx
+++ farcaster/app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "../components/ThemeProvider";
 import "./globals.css";
 import { Metadata } from "next";
 import { Toaster } from "../components/ui/sonner";
+import { ClaimProvider } from '../context/utilityProvider/ClaimContextProvider';
+
 
 const appUrl = process.env.NEXT_PUBLIC_URL
 export const metadata: Metadata = {
@@ -49,16 +51,18 @@ export default async function RootLayout({
       <body>
         <ThemeProvider>
           <Providers session={session}>
-            <div className="min-h-screen bg-gradient-radial from-white via-gray-50 to-gray-100 dark:from-black dark:via-black dark:to-black">
-              <Header />
-              <main className="py-6 px-4 sm:px-6 lg:px-8">
-                <div className="max-w-7xl mx-auto">
-                  {children}
-                </div>
-              </main>
-                      <Toaster />
-              <Footer />
-            </div>
+            <ClaimProvider>
+              <div className="min-h-screen bg-gradient-radial from-white via-gray-50 to-gray-100 dark:from-black dark:via-black dark:to-black">
+                <Header />
+                <main className="py-6 px-4 sm:px-6 lg:px-8">
+                  <div className="max-w-7xl mx-auto">
+                    {children}
+                  </div>
+                </main>
+                <Toaster />
+                <Footer />
+              </div>
+            </ClaimProvider>
           </Providers>
         </ThemeProvider>
       </body>
