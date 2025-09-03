8e285d5b83327d88c08ee93f058df8e7fd169a64
Anthony Nweke <75612396+Anthonyslav1@users.noreply.github.com>
2025-09-03 12:00:01 +0100

    naming error fix

--- backend/lib/services/notificationService.ts
+++ backend/lib/services/notificationService.ts
@@ -9,7 +9,7 @@ const emailTransporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE || 'gmail', // or your preferred email service
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASSWORD
   }
 });
 
